cb16f41c7bce6d6e48c685a7010ab5ab3ccabfed
Алексей Гардер <aleksey_garder@outlook.com>
2019-06-22 20:36:13 +0700

    Alpha version

--- Shared/Utilities/Constants.cs
+++ Shared/Utilities/Constants.cs
@@ -6,10 +6,10 @@ namespace Shared.Utilities
 {
     public static class Constants
     {
-        public static readonly string currentRoomsStatusURL = "http://10.0.2.2:5000/api/roomslastinfo";
-        public static readonly string currentRoomStatusURL = "http://10.0.2.2:5000/api/roomslastinfo/{0}";
-        public static readonly string currentBoilerStatusURL = "http://10.0.2.2:5000/api/boilerlastinfo";
-        public static readonly string quickInfoURL = "http://10.0.2.2:5000/api/QuickInfo";
+        public static readonly string currentRoomsStatusURL = "http://192.168.43.253:5000/api/roomslastinfo";
+        public static readonly string currentRoomStatusURL = "http://192.168.43.253:5000/api/roomslastinfo/{0}";
+        public static readonly string currentBoilerStatusURL = "http://192.168.43.253:5000/api/boilerlastinfo";
+        public static readonly string quickInfoURL = "http://192.168.43.253:5000/api/QuickInfo";
         public static readonly string defaultUserName = "default";
     }
 }
